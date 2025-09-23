fdb1404bd79013e3f7901f79b134df2afb2d041a
Hiroki Kurosawa <s@vemi.jp>
2025-09-23 14:30:13 +0900

    fix: 出力ファイル名修正

--- git_diff_with_untracked.sh
+++ git_diff_with_untracked.sh
@@ -1,1 +1,1 @@
-git ls-files --others --exclude-standard -z | xargs -0 -I{} git add -N "{}" && git diff > .diff
+git ls-files --others --exclude-standard -z | xargs -0 -I{} git add -N "{}" && git diff > diff.diff
